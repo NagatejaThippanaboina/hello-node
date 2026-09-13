f66f0c5ce8cbc53f3ab382bd2e2bf8b7cd3971ef
NagatejaThippanaboina <nagateja574@gmail.com>
2026-09-13 21:02:44 +0530

    Complete HTTP server and registration page

--- index.js
+++ index.js
@@ -1,1 +1,48 @@
-console.log("Hello Node.js!");
+const http = require("http");
+const fs = require("fs");
+const path = require("path");
+
+const args = process.argv;
+
+let port = 3000;
+
+const portIndex = args.indexOf("port");
+
+if (portIndex !== -1 && args[portIndex + 1]) {
+  port = Number(args[portIndex + 1]);
+}
+
+const server = http.createServer((req, res) => {
+  let fileName;
+
+  if (req.url === "/") {
+    fileName = "home.html";
+  } else if (req.url === "/home") {
+    fileName = "home.html";
+  } else if (req.url === "/project") {
+    fileName = "project.html";
+  } else if (req.url === "/registration") {
+    fileName = "registration.html";
+  } else {
+    res.writeHead(404, { "Content-Type": "text/plain" });
+    res.end("404 - Page Not Found");
+    return;
+  }
+
+  const filePath = path.join(__dirname, fileName);
+
+  fs.readFile(filePath, (error, data) => {
+    if (error) {
+      res.writeHead(500, { "Content-Type": "text/plain" });
+      res.end("500 - Internal Server Error");
+      return;
+    }
+
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.end(data);
+  });
+});
+
+server.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
